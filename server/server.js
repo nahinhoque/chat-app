38385ecf021a392bdcce17b85bd77d33038f087b
Nahin Hoque <abunahinibn@gmail.com>
2025-09-09 05:51:46 +0600

    server.js updated for vercel

--- server/server.js
+++ server/server.js
@@ -53,5 +53,10 @@ app.use("/api/messages", messageRouter)
 // connect to mongodb
 await connectDB();
 
+if(process.env.NODE_ENV !== "production"){
 const PORT = process.env.PORT || 5000;
 server.listen(PORT, ()=> console.log('Server is running: ' + PORT));
+}
+
+// export server for vercel
+export default server;
